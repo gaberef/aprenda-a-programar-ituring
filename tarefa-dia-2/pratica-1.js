37393287a16477c457c9eb8b49e78b4a53a4638f
gaberef <gabiniger@otmail.com>
2022-08-25 19:24:09 -0300

    pratica-1.vscode

---
+++ tarefa-dia-2/pratica-1.js
@@ -0,0 +1,12 @@
+// let seu nome
+// let sua profissão
+// let seu email
+// let sua idade
+// let o lugar que você sonha um dia conhecer (pode ser um país, cidade ou um local mesmo)
+
+let Seu nome =  "Gabriel"
+let Profissão = "Desenvolvedor"
+let Emmail = "gabiniger@hotmail.com"
+let Idae = "25 anos"
+let lugar que você sonha um dia conhecer = "Fortaleza"
+
