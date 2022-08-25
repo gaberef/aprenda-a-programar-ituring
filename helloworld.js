46c1c7b1436fe805047a1bd70dba338d4b300938
gaberef <gabiniger@otmail.com>
2022-08-25 18:58:48 -0300

    tarefa-dia-1-adc

--- helloworld.js
+++ helloworld.js
@@ -1,3 +1,38 @@
 console.log("Hello World!")
 console.log("Olá Mundo!")
 console.log("안녕하세요 세계")
+
+//comentario em javascript
+
+let minhaVariavel
+let outraVariavel = 1
+
+minhaVariavel = "Ituring"
+
+console.log(minhaVariavel)
+// console.log(outraVariavel)
+
+// opreradores matematicos
+//+ - * /
+
+let numero1= 100
+let numero2 = 300
+
+console.log(numero1 + numero2)
+
+
+let texto1 = "Ituring - "
+let texto2 = "aprender a programar"
+let resultado = texto1 + texto2
+
+console.log(texto1)
+console.log(texto2)
+console.log(resultado)
+
+
+let contaDoBar = 1000
+let porcetagememGorjeta = 10
+let gorjetadoGarcom = contaDoBar + porcetagememGorjeta/100
+
+console.log("A gorjeta do garcom é igual a:")
+console.log(gorjetadoGarcom + "reais")
